39c06437c5510baed22a62d4915b557633a46910
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-02-13 15:40:36 -0800

    done

---
+++ DemoConcord/IDepartmentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConcord
+{
+    internal interface IDepartmentRepository
+    {
+
+        internal interface IDepartmentRepository
+        {
+            IEnumerable<Department> GetAllDepartments();
+        }
+    }
+}
---
+++ DemoConcord/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConcord
+{
+    internal class Product
+    {
+        public String Name { get; set; }
+        public double Price { get; set; }
+
+        public int CategoryID { get; set; }
+
+    }
+}
